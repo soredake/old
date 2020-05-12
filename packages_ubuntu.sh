e49f3712da720ab3264aeabe8db09baaf6870a21
soredake <fdsfgs@krutt.org>
2020-05-12 15:23:03 +0300

    update

--- packages_ubuntu.sh
+++ packages_ubuntu.sh
@@ -1,42 +1,14 @@
 #!/bin/bash
 plasma=(
-  anime-downloader-git # pip3
   boxtron/roberta/luxtorpeda # tar/wget
-  dosbox-staging # https://snapcraft.io/dosbox-staging
   dupeguru # https://github.com/arsenetar/dupeguru/issues/484
   etcher # appimage
   flips # https://aur.archlinux.org/packages/flips-git
-  gb-studio-bin # direct deb
-  itch # lutris
-  kde-cdemu-manager-kf5 # gcdemu
-  cdemu-client vhba-module-dkms # https://launchpad.net/~cdemu/+archive/ubuntu/ppa
-  kde-service-menu-reimage # https://store.kde.org/p/1231579/
-  kwin-lowlatency # https://launchpad.net/~maxiberta/+archive/ubuntu/kwin-lowlatency
-  lector # another reader?
   libstrangle-git # lutris
   mpv-webm-git # wget
-  multimc5 # direct tarball
-  ninfs # pip
-  pkgtop # https://github.com/orhun/pkgtop/issues/13
-  proton-ge-custom-bin # tar
-  protonfixes-git # pip
-  protontricks # pipx
-  pulseaudio-modules-bt-git # ppa
   ripme # java
-  rpcs3-bin # appimage
   samrewritten-git # appimage
   steascree # direct tarball
-  syncplay # snap
-  syobon # snap
-  kde-config-systemd
-  fancontrol-kcm
-  https://launchpad.net/~mc3man/+archive/ubuntu/mpv-tests?field.series_filter=eoan
-  taisei # https://flathub.org/apps/details/org.taisei_project.Taisei
-  tldr++ # another client
-  trackma-git
-  viber # snap
   vita3k-git # direct tarball
   vkbasalt # direct tarball
-  wine-mono-bin wine-gecko
-  yuzu-mainline/citra-git # ppa/flatpak
 )
