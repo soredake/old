b23ed2ddafc4276ed878b03ee2878e25f859fe48
soredake <5204968+soredake@users.noreply.github.com>
2021-05-01 11:53:43 +0300

    vsdf

--- setup-fedora.sh
+++ setup-fedora.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-sudo dnf copr enable dawid/better_fonts -y
 echo -e "fastestmirror=True\nmax_parallel_downloads=10"|sudo tee -a /etc/dnf/dnf.conf
+sudo dnf copr enable dawid/better_fonts -y
 # Install my packages
 # https://pagure.io/fedora-kde/SIG/issue/52
-# preinstall plasma-disks
-# preinstall pulseaudio-module-gsettings https://pagure.io/fedora-kde/SIG
+# preinstall plasma-disks https://pagure.io/fedora-kde/SIG
+# https://bugzilla.redhat.com/show_bug.cgi?id=1821621 https://src.fedoraproject.org/rpms/dnf/pull-request/19
 sudo dnf install -y https://github.com/rpmsphere/noarch/raw/master/r/rpmsphere-release-$(rpm -E %fedora)-1.noarch.rpm https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 packages=(fontconfig-font-replacements fontconfig-enhanced-defaults https://www.vpn.net/installers/logmein-hamachi-2.1.0.203-1.x86_64.rpm https://www.thefanclub.co.za/sites/default/files/public/overgrive/overgrive-3.3.9.noarch.rpm dnf-plugin-system-upgrade android-tools aria2 bleachbit chntpw dolphin-plugins filelight fish gamemode gimp lm_sensors lutris mpv plasma-discover-snap qdirstat rclone-browser retroarch seahorse steam stow vitetris wine-devel simplescreenrecorder git icoutils)
-packages+=(libappindicator-gtk3 python3-psutil cairo-devel python3-devel gobject-introspection-devel cairo-gobject-devel)
+packages+=(libappindicator-gtk3 python3-psutil cairo-devel python3-devel gobject-introspection-devel cairo-gobject-devel) # safeeyes
 sudo dnf install -y ${packages[@]}
-sudo dnf groupupdate -y core sound-and-video
-sudo dnf groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf groupupdate -y core sound-and-video multimedia
 
 # https://snapcraft.io/docs/installing-snap-on-fedora
-sudo systemctl enable snapd
+sudo systemctl start snapd
 sudo ln -s /var/lib/snapd/snap /snap
 
 sudo snap install code --classic
@@ -25,7 +24,7 @@ sudo flatpak override org.jdownloader.JDownloader --filesystem=host
 pip install -U git+https://github.com/simons-public/protonfixes protontricks internetarchive safeeyes
 fish -c 'curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher pure-fish/pure'
 aria2c -c -d "$HOME/.config/mpv/scripts" https://github.com/ekisu/mpv-webm/releases/download/latest/webm.lua
-sudo gtk-update-icon-cache /usr/share/icons/hicolor # safeeyes
+#sudo gtk-update-icon-cache /usr/share/icons/hicolor # safeeyes
 
 # setup dofiles
 etc_cp/install.sh
