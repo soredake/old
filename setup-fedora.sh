15ab390d037abceb000794cf450ba6242bf7aae3
soredake <5204968+soredake@users.noreply.github.com>
2022-03-26 19:10:25 +0200

    gdfg

--- setup-fedora.sh
+++ setup-fedora.sh
@@ -4,9 +4,9 @@
 sudo dnf copr enable zawertun/hack-fonts -y # https://bugzilla.redhat.com/show_bug.cgi?id=1258542
 # Install my packages
 # TODO: icoutils(needed for exe preview) as dep for wine https://src.fedoraproject.org/rpms/wine/blob/rawhide/f/wine.spec or dolphin
-sudo dnf install -y https://github.com/rpmsphere/noarch/raw/master/r/rpmsphere-release-$(rpm -E %fedora)-1.noarch.rpm https://mirrors.rpmfusion.org/{free/fedora/rpmfusion-,nonfree/fedora/rpmfusion-non}free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y https://github.com/rpmsphere/noarch/raw/master/r/rpmsphere-release-34-2.noarch.rpm https://mirrors.rpmfusion.org/{free/fedora/rpmfusion-,nonfree/fedora/rpmfusion-non}free-release-$(rpm -E %fedora).noarch.rpm
 packages=(https://www.vpn.net/installers/logmein-hamachi-2.1.0.203-1.x86_64.rpm https://www.thefanclub.co.za/sites/default/files/public/overgrive/overgrive-3.3.10.noarch.rpm https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher-2.2.0-travis995.0f91801.x86_64.rpm python3-dnf-plugin-system-upgrade bottles android-tools aria2 bleachbit chntpw fish gimp lm_sensors hack-fonts lutris mpv plasma-discover-snap qdirstat rclone-browser retroarch seahorse steam stow vitetris wine trash-cli safeeyes)
-#packages+=(libappindicator-gtk3 python3-psutil cairo-devel python3-devel gobject-introspection-devel cairo-gobject-devel) # safeeyes https://github.com/slgobinath/SafeEyes/issues/432
+packages+=(libappindicator-gtk3 python3-psutil cairo-devel python3-devel gobject-introspection-devel cairo-gobject-devel) # safeeyes https://github.com/slgobinath/SafeEyes/issues/432
 sudo dnf install -y ${packages[@]}
 # https://rpmfusion.org/Configuration
 # https://fedoramagazine.org/things-to-do-after-installing-fedora-34-workstation/
@@ -17,21 +17,21 @@ sudo dnf groupupdate core sound-and-video multimedia
 sudo systemctl start snapd
 sudo ln -s /var/lib/snapd/snap /snap
 
-sudo snap install code --classic
+sudo snap install code --classic # https://github.com/microsoft/vscode/issues/141788 https://pagure.io/fedora-workstation/issue/283
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install -y flathub org.jdownloader.JDownloader com.viber.Viber com.github.ztefn.haguichi com.spotify.Client com.discordapp.Discord com.github.micahflee.torbrowser-launcher com.mojang.Minecraft net.rpcs3.RPCS3 org.telegram.desktop org.freefilesync.FreeFileSync
 sudo flatpak override --filesystem=xdg-config/fontconfig:ro # https://github.com/flatpak/flatpak/issues/3947
 sudo flatpak override org.jdownloader.JDownloader --filesystem=host # https://github.com/flathub/org.jdownloader.JDownloader/pull/19
-pip install -U git+https://github.com/simons-public/protonfixes protontricks internetarchive #safeeyes
+pip install --user git+https://github.com/simons-public/protonfixes protontricks internetarchive safeeyes
 fish -c 'curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher pure-fish/pure'
 #aria2c -c -d "$HOME/.config/mpv/scripts" https://github.com/ekisu/mpv-webm/releases/download/latest/webm.lua
-#sudo gtk-update-icon-cache /usr/share/icons/hicolor # safeeyes
+sudo gtk-update-icon-cache /usr/share/icons/hicolor # safeeyes
 
 # setup dofiles
 etc_cp/install.sh
 home/install.sh
 
 # general settings
-sudo ln -sfv "$HOME/.config/fontconfig/fonts.conf" /etc/fonts/local.conf # https://bugs.launchpad.net/snapd/+bug/1916867
+sudo ln -sfv "$HOME/.config/fontconfig/fonts.conf" /etc/fonts/local.conf # https://bugs.launchpad.net/snapd/+bug/1916867 https://bugzilla.mozilla.org/show_bug.cgi?id=1760996
 sudo tee -a /usr/share/sddm/scripts/Xsetup <<< "xmodmap /home/danet/git/dotfiles_home/home/xmodmap/.Xmodmap"
 #sed -e '$aHidden=True' /etc/xdg/autostart/org.kde.discover.notifier.desktop > "$HOME/.config/autostart/org.kde.discover.notifier.desktop" # https://bugs.kde.org/show_bug.cgi?id=413053
