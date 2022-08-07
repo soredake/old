fa31de660478092461d1e1c9157068b9e3d72030
soredake <5204968+soredake@users.noreply.github.com>
2022-08-07 21:08:19 +0300

    hjhjk

--- setup-fedora.sh
+++ setup-fedora.sh
@@ -7,10 +7,10 @@ sudo dnf copr enable zawertun/hack-fonts -y # https://bugzilla.redhat.com/show_b
 # lutris/steam flatpak blockers: https://github.com/flathub/net.lutris.Lutris/issues/198 https://github.com/flathub/com.valvesoftware.Steam/issues/770 https://github.com/PaulCombal/SamRewritten/issues/128
 #sudo dnf remove -y 
 sudo dnf install -y https://github.com/rpmsphere/noarch/raw/master/r/rpmsphere-release-36-1.noarch.rpm https://mirrors.rpmfusion.org/{free/fedora/rpmfusion-,nonfree/fedora/rpmfusion-non}free-release-$(rpm -E %fedora).noarch.rpm
-packages=(https://www.vpn.net/installers/logmein-hamachi-2.1.0.203-1.x86_64.rpm https://www.thefanclub.co.za/sites/default/files/public/overgrive/overgrive-3.4.3.noarch.rpm nodejs btrfsmaintenance btrfs-assistant python3-dnf-plugin-system-upgrade chntpw fish lm_sensors hack-fonts lutris mpv plasma-discover-snap qdirstat rclone steam-devices steam stow vitetris wine safeeyes syncplay code protontricks tor steamtinkerlaunch mangohud vkbasalt gamescope)
+packages=(https://www.vpn.net/installers/logmein-hamachi-2.1.0.203-1.x86_64.rpm https://www.thefanclub.co.za/sites/default/files/public/overgrive/overgrive-3.4.3.noarch.rpm bottles nodejs btrfsmaintenance btrfs-assistant python3-dnf-plugin-system-upgrade chntpw fish lm_sensors hack-fonts lutris mpv plasma-discover-snap qdirstat rclone steam-devices steam stow vitetris wine safeeyes syncplay code protontricks tor steamtinkerlaunch mangohud vkbasalt gamescope)
 sudo dnf install -y ${packages[@]}
 sudo dnf group install core sound-and-video multimedia # https://rpmfusion.org/Configuration
-flatpak install -y flathub com.usebottles.bottles org.jdownloader.JDownloader com.viber.Viber com.github.ztefn.haguichi com.spotify.Client com.discordapp.Discord com.github.micahflee.torbrowser-launcher net.rpcs3.RPCS3 net.davidotek.pupgui2 org.gnome.seahorse.Application com.github.maoschanz.drawing org.gimp.GIMP org.keepassxc.KeePassXC org.qbittorrent.qBittorrent org.zealdocs.Zeal com.parsecgaming.parsec org.ppsspp.PPSSPP com.steamgriddb.steam-rom-manager org.libretro.RetroArch
+flatpak install -y flathub org.jdownloader.JDownloader com.viber.Viber com.github.ztefn.haguichi com.spotify.Client com.discordapp.Discord com.github.micahflee.torbrowser-launcher net.rpcs3.RPCS3 net.davidotek.pupgui2 org.gnome.seahorse.Application com.github.maoschanz.drawing org.gimp.GIMP org.keepassxc.KeePassXC org.qbittorrent.qBittorrent org.zealdocs.Zeal com.parsecgaming.parsec org.ppsspp.PPSSPP com.steamgriddb.steam-rom-manager org.libretro.RetroArch
 pip install --user internetarchive
 fish -c 'curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher pure-fish/pure'
 wget -P "$HOME/.config/mpv/scripts" https://github.com/ekisu/mpv-webm/releases/download/latest/webm.lua
@@ -27,7 +27,7 @@ chmod +x appimaged-*.AppImage
 wget -c https://github.com/$(wget -q https://github.com/TDesktop-x64/tdesktop/releases -O - | grep "64Gram.*_linux.zip" | head -n 1 | cut -d '"' -f 2)
 unzip 64* -d 64gram
 ./64gram/Telegram
-# TODO: itch(https://github.com/itchio/itch/issues/2793) steascree(https://github.com/awthwathje/SteaScree/issues/35) emusak-ui(TODO: request flatpak)
+# TODO: itch(https://github.com/itchio/itch/issues/2793) steascree(https://github.com/awthwathje/SteaScree/issues/35)
 
 etc_cp/install.sh; home/install.sh
 
